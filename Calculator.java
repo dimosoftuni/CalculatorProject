5ab8c8be56200a092101c25f7faade2df64c804d
Gandalf <157281072+dimofeevqa@users.noreply.github.com>
2025-01-15 18:22:56 +0200

    new change

--- Calculator.java
+++ Calculator.java
@@ -30,6 +30,7 @@ public class Calculator {
 
     // Main method to demonstrate usage
     // some change ot Gandalf
+    // new change
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
